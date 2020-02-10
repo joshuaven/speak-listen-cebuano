eae2729584c0f6cec58ae77b2f40b5a468afdac9
NishaAlexis <alexispadura@gmail.com>
2020-02-10 19:30:49 +0800

    added listen page

--- public/scripts/sentence.js
+++ public/scripts/sentence.js
@@ -11,9 +11,22 @@ var sentences = [
   "Mangaon ta ninyo!"
 ];
 
+var english = [
+  "Can you speak in English?",
+  "Where is Ayala located?",
+  "Which jeepney should I ride to get to the pier?",
+  "Good Morning! How are you?",
+  "Thank you very much!",
+  "You're welcome!",
+  "I don't know!",
+  "How much is this?",
+  "Please speak slowly",
+  "Let's eat!"
+]
 
 function newSentence() {
   var randomNumber = Math.floor(Math.random() * sentences.length);
   document.getElementById("lineDisplay").innerHTML = sentences[randomNumber];
   document.getElementById("lineNumber").innerHTML = randomNumber;
+  document.getElementById("english").innerHTML = "Translation: " + english[randomNumber];
 }
