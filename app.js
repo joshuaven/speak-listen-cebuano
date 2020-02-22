2239bb376305fb5dbad0684527c93cefa3685b83
JoshuaVen <youzeiteikoku@gmail.com>
2020-02-23 01:37:51 +0800

    added winston and loggers

--- app.js
+++ app.js
@@ -4,14 +4,44 @@ const multer = require("multer");
 const upload = multer();
 const fs = require("fs");
 const path = require("path");
+const { createLogger, format, transports } = require("winston");
+
+const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss"
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.simple()
+  ),
+  defaultMeta: { service: "speak-listen-cebuano" },
+  transports: [
+    // new transports.Console({ format: format.simple() }),
+    new transports.File({ filename: "quick-start-error.log", level: "error" }),
+    new transports.File({ filename: "quick-start-combined.log" })
+  ]
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new transports.Console({
+      format: format.simple()
+    })
+  );
+}
 
 app.get("/listFiles", (req, res, next) => {
   const directoryPath = path.join(__dirname, "public/uploads");
+  logger.log("info", "fetching directory: %s", "./public/uploads");
 
   fs.readdir(directoryPath, (err, files) => {
     if (err) {
-      console.log("Error getting directory");
+      logger.log("error", new Error("Error getting data from directory!"));
+      // console.log("Error getting directory");
     } else {
+      logger.info("Files loaded successfully!");
       res.send(files);
     }
   });
@@ -27,11 +57,14 @@ app.post("/upload", upload.single("soundBlob"), function(req, res, next) {
     Buffer.from(new Uint8Array(req.file.buffer))
   );
 
+  logger.info("File uploaded to server successfully!");
   res.sendStatus(200);
 });
 
+logger.info("Loading HTTP files in public folder");
 app.use(express.static("public"));
 
 app.listen(3000, function() {
-  console.log("app listening to port 3000");
+  logger.log("info", "app listening to port 3000");
+  // console.log("app listening to port 3000");
 });
